ebf0c99cf3da767cddcec889d941ab97f45132c2
Leo Moon <LeoM@skyglobal.com>
2020-04-30 21:19:13 -0700

    Project setup and coordinator

---
+++ MVVMRxSwift/AppCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinator.swift
+//  MVVMRxSwift
+//
+//  Created by Leo Moon on 2020-04-30.
+//  Copyright © 2020 Leo Moon. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator {
+    
+    private let window: UIWindow
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let viewController = ViewController.instantiate()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
